0f45fa3eb9abe21d1e775d4a36ce8ff6d60ec58b
Matthew Conlen <mc@mathisonian.com>
2015-10-08 21:04:57 -0400

    rename to normal leaflet + js conventions. add loader factory

---
+++ src/index.js
@@ -0,0 +1,21 @@
+(function (factory, window) {
+    // define an AMD module that relies on 'leaflet'
+    if (typeof define === 'function' && define.amd) {
+        define(['leaflet'], factory);
+
+    // define a Common JS module that relies on 'leaflet'
+    } else if (typeof exports === 'object') {
+        module.exports = factory(require('leaflet'));
+    }
+
+    // attach your plugin to the global 'L' variable
+    if (typeof window !== 'undefined' && window.L) {
+        window.L.FreeDraw = factory(L);
+    }
+}(function (L) {
+    var FreeDraw = {};
+    // implement your plugin
+
+    // return your plugin when you are done
+    return FreeDraw;
+}, window));
